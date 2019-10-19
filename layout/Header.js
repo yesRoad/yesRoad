bff81bb6ca5b927f3706a8fcbd636ac13f9b8e9a
kyh2606 <keg2606@gmail.com>
2019-10-19 22:53:47 +0900

    메뉴 추가

--- layout/Header.js
+++ layout/Header.js
@@ -8,6 +8,14 @@ const Header = () => (
       <span className="span01" />
       <span className="span02" />
       <span className="span03" />
+
+      <div className="menu-area">
+        <ul>
+          <li><a href="#none">Menu01</a></li>
+          <li><a href="#none">Menu02</a></li>
+          <li><a href="#none">Menu03</a></li>
+        </ul>
+      </div>
     </nav>
   </header>
 );
