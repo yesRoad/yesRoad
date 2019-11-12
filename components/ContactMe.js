9e4fef65ffef6551e5662ca3037b255fec86b4ef
kyh2606 <eunggilkim@Eunggilui-iMac.local>
2019-11-12 20:34:04 +0900

    메일 폼 추가

--- components/ContactMe.js
+++ components/ContactMe.js
@@ -4,7 +4,16 @@ import '@styles/layout/contactMe.scss';
 const ContactMe = () => (
   <>
     <section>
-        <span>라우터어어어어어어어ㅓ어어엉</span>
+      <article className="form">
+        <h3>Contact Me</h3>
+        <form>
+          <input type="text" name="first_name" className="input-text" id="first_name" placeholder="Name" required="" />
+          <input type="email" className="input-text" name="email" id="email" placeholder="Email" required="" />
+          <input type="tel" className="input-text" name="telephone" id="telephone" placeholder="Phone" />
+          <textarea className="input-text text-area" name="comments" id="message" rows="5" placeholder="Message"></textarea>
+          <div className="text-center"><button type="submit" className="input-btn">Send Message</button></div>
+        </form>
+      </article>
     </section>
   </>
 );
